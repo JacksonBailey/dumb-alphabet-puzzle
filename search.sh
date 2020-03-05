217379e296689ffcbe2e177573372bf2a3f7b1f6
Jackson Bailey <jackson.bailey@ondotsystems.com>
2020-03-05 16:03:01 -0500

    Eliminate primes based on length of longest word

--- search.sh
+++ search.sh
@@ -2,6 +2,19 @@
 
 set -euo pipefail
 
+# TODO Manually find primes, for now hard code ones less than 100
+primes=( 2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97 )
+
 max_length=$(awk 'length > max_length { max_length = length; longest_line = $0 } END { print max_length }' ./words_alpha.txt)
 
 echo "Max length: ${max_length}"
+
+possible_primes=()
+
+for x in "${primes[@]}"; do
+    if [ $(( x * 2 )) -le ${max_length} ]; then
+        possible_primes+=($x)
+    fi
+done
+
+echo "Possible primes based on that: ${possible_primes[*]}"
