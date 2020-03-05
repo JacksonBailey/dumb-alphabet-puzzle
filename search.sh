f36fcc6f69ec30d6912952bc68d2a6e5d39f8c5c
Jackson Bailey <jackson.bailey@ondotsystems.com>
2020-03-05 15:40:19 -0500

    Max length of a word

--- search.sh
+++ search.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+
+set -euo pipefail
+
+max_length=$(awk 'length > max_length { max_length = length; longest_line = $0 } END { print max_length }' ./words_alpha.txt)
+
+echo "Max length: ${max_length}"
